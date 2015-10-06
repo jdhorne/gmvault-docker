7da569986d41ece707852b4e4b4f51220d582425
Jason Horne <jason@drjason.com>
2015-10-06 14:58:16 -0400

    next working version; now automatically runs gmvault if OAuth token is present

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,23 @@
 FROM alpine:latest
 
-ENV GMVAULT_DIR /config
+ENV GMVAULT_DIR /data
 ENV GMVAULT_EMAIL_ADDR test@example.com
  
+RUN echo "@testing http://nl.alpinelinux.org/alpine/edge/testing" >> /etc/apk/repositories
 RUN apk add --update \
-        bash \
+	bash \
         ca-certificates \
         python \
         py-pip \
+        shadow@testing \
+    && pip install --upgrade pip \
     && pip install gmvault \
-    && rm -rf /var/cache/apk/*
+    && rm -rf /var/cache/apk/* \
+    && addgroup abc \
+    && adduser -s /bin/bash -G abc -H -D abc
+
     
-VOLUME /db /config
+VOLUME /data
 RUN mkdir /app
 COPY gmvault.sh /app/gmvault.sh
 
--- gmvault.sh
+++ gmvault.sh
@@ -1,13 +1,34 @@
 #!/bin/bash
-export CMD=gmvault sync -d /db $GMVAULT_EMAIL_ADDR
+
+OAUTH_TOKEN="/data/${GMVAULT_EMAIL_ADDR}.oauth2"
+
+
+if [ ! "$(id -u abc)" -eq "$PUID" ]; then usermod -o -u "$PUID" abc ; fi
+if [ ! "$(id -g abc)" -eq "$PGID" ]; then groupmod -o -g "$PGID" abc ; fi
+
+chown -R abc:abc /data
+
+if [ -f $OAUTH_TOKEN ]; then
+	echo OAuth token located: $OAUTH_TOKEN
+	echo running gmvault
+	su -c "gmvault sync -d /data ${GMVAULT_EMAIL_ADDR}" abc
+	exit
+fi
+
 
 echo #####
 echo # FIRST RUN SETUP
-echo You must provide your GMail credentials using OAuth. Attach to this container and run the following command interactively:
-echo    $CMD
-echo Once you provide an authentication token, the synchronization will start. Control-C to stop that, then edit the file 
+echo #####
+echo
+echo OAuth token could not be located \($OAUTH_TOKEN\)
+echo You must provide your GMail credentials using OAuth.
+echo Attach to this container and run the following command interactively:
+echo
+echo "   su -c \"gmvault sync -d /data ${GMVAULT_EMAIL_ADDR}\" abc"
+echo
+echo Once you provide an authentication token, the synchronization will start.
+echo Control-C to stop that, then restart this container.
+echo
+echo Lacking the capability to anything more, pausing now for dramatic effect.
 
-/bin/bash
-
-echo Backing up $GMVAULT_EMAIL_ADDR
-$CMD
+/bin/sh
